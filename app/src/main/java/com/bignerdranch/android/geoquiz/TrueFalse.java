9f80a98a52811ca34d63d774dec708ee154a2bdb
ppalumbo17 <ppalumbo@mines.edu>
2016-01-22 13:54:31 -0700

    Implemented Question Class Partial

--- app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -3,13 +3,19 @@ package com.bignerdranch.android.geoquiz;
 /**
  * Created by Peter on 1/20/2016.
  */
-public class TrueFalse {
+public class TrueFalse {//extends Question {
     private int mQuestion;
     private boolean mTrueQuestion;
+
     public TrueFalse(int question, boolean trueQuestion) {
         mQuestion = question;
         mTrueQuestion = trueQuestion;
     }
+
+    /*public TrueFalse(int textResId, boolean answerTrue) {
+        super(textResId, answerTrue);
+    }*/
+
     public int getQuestion() {
         return mQuestion;
     }
