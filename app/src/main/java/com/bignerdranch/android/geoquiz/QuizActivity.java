c82980850cace48afb11a629153112fc6dd4acde
Peter Palumbo <ppalumbo@mines.edu>
2016-01-27 15:44:50 -0700

    First Commit

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,8 @@ import android.widget.Toast;
 
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity.java";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -54,6 +57,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -279,5 +283,33 @@ public class QuizActivity extends AppCompatActivity {
         mFourthButton.setVisibility(View.INVISIBLE);
         mFreeResponse.setVisibility(View.INVISIBLE);
         mSubmitButton.setVisibility(View.INVISIBLE);
+    }
+
+
+    //Logs
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
